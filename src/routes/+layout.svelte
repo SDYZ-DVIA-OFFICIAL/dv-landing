4d25cfe71451ef74c92aa793d2de5dd1fadba513
HYH! <huangyinhaow@outlook.com>
2023-07-26 15:30:51 +0800

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script>
+	import '../app.css';
+	import logo from '$lib/images/DV-DARK-LOGO.svg';
+</script>
+
+<div class="main">
+	<div class="nav">
+		<div class="icon"><img src={logo} alt="icon" /></div>
+		<div class="name">The Digital Video Intelligence Association</div>
+	</div>
+	<div class="content"><slot /></div>
+	<div class="footer">
+		© DVIA 2006 - {new Date().getFullYear()}
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		@apply m-0 p-0;
+	}
+	:global(body) {
+		font-family: 'IBM Plex Sans', sans-serif;
+		@apply min-h-screen bg-red-50 text-black;
+	}
+	.main {
+		@apply w-full lg:pl-32 lg:pr-32;
+	}
+	.nav {
+		@apply mb-4 flex h-24 w-full flex-row flex-nowrap items-center bg-red-100;
+	}
+	.nav > div {
+		@apply ml-4 mr-4;
+	}
+	img {
+		@apply h-full max-h-full pb-4 pt-4;
+	}
+	.icon {
+		@apply h-full max-h-full;
+	}
+	.name {
+		@apply text-xl;
+	}
+	.content {
+		@apply ml-4 mr-4;
+	}
+	.footer {
+		@apply ml-2 mr-2 mt-4;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+	import 'remixicon/fonts/remixicon.css';
+	import icon from '$lib/images/DV-LOGO.svg';
+</script>
+
+<div class="main">
+	<div class="head">
+		<div class="text">
+			<div class="motto">The place where your dreams come true.</div>
+			<div class="detail">
+				Founded in 2006, DVIA is one of the largest and oldest digital video associations in the
+				southern part of the People's Republic of China.
+			</div>
+			<div class="links">
+				<div class="tabs">
+					<a href="https://space.bilibili.com/473013658" target="_blank"
+						><i class="ri-bilibili-line" />bilibili</a
+					>
+					<a href="/" target="_blank"><i class="ri-tiktok-line" />Douyin</a>
+					<a href="https://www.youtube.com/@SDYZDVIA"><i class="ri-youtube-line" />YouTube</a>
+					<a href="/" target="_blank"><i class="ri-telegram-line" />Telegram</a>
+					<a href="/" target="_blank"><i class="ri-tiktok-line" />Tiktok</a>
+				</div>
+			</div>
+		</div>
+		<div class="icon"><img src={icon} alt="icon" /></div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.head {
+		@apply flex w-full flex-nowrap content-between justify-between lg:mb-10 lg:mt-10;
+	}
+	.icon {
+		@apply hidden lg:block;
+	}
+	.icon > img {
+		@apply inline-block w-full;
+	}
+	.text {
+		@apply w-full grow pb-2 pt-2 lg:w-2/3 lg:pr-8 lg:text-left;
+	}
+	.motto {
+		@apply mb-4 text-4xl font-semibold capitalize lg:text-5xl;
+	}
+	.detail {
+		@apply mb-4 mt-4 text-lg font-normal;
+	}
+	.tabs > a {
+		@apply tab tab-active text-lg text-black;
+	}
+	i {
+		@apply pl-1 pr-1;
+	}
+</style>
